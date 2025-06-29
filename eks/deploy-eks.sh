4e4dd3864f751b5aff39cab8c3b644424d1fe580
Ed Sweeney <ed@onextent.com>
2025-06-29 15:55:48 -0700

    bullshit

--- eks/deploy-eks.sh
+++ eks/deploy-eks.sh
@@ -10,7 +10,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Configuration
-CLUSTER_NAME="audio-lab-cluster"
+CLUSTER_NAME="audio-lab-cluster-2"
 REGION="${AWS_REGION:-us-east-1}"
 SUBDOMAIN="${SUBDOMAIN:-audiolab}"
 BASE_DOMAIN="${BASE_DOMAIN:-example.com}"
